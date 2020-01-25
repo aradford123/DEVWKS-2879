1e3663947ee8dd454efaafa49bfd8b90a8551a73
Adam Radford (aradford) <aradford@cisco.com>
2020-01-26 01:54:16 +1100

    CLEUR20

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 git clone https://github.com/CiscoDevNet/DNAC-Platform.git
 pip install -r DNAC-Platform/WebHookServer/requirements.txt
 
+git clone https://github.com/CiscoDevNet/DNAC-Subscription.git
+
